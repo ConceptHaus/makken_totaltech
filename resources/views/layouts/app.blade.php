aeeede1ab3435eef206e28054938bfbb284105d4
Lizbeth Luna <liz@concepthaus.mx>
2018-09-13 19:07:40 -0500

    Se limpio app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,19 +10,6 @@
 
     <!-- Estilos -->
    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-     <!-- Bootstrap -->
-   <link rel="stylesheet" href="{{ asset('css/bootstrap-grid.min.css') }}">
-   <link rel="stylesheet" href="{{ asset('css/bootstrap-reboot.min.css') }}">
-   <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-   <!-- Latest compiled and minified CSS -->
-{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> --}}
-   {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> --}}
-   <!-- Fin de estilos -->
-
-
-    <!--[if lt IE 9]>
-      <script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
 
 </head>
 
@@ -82,13 +69,8 @@ Términos y Condiciones
 <!-- Fin Footer -->
 
   <!-- Scripts -->
-   <!-- Bootstrap -->
-  <script src="{{ asset('js/bootstrap.bundle.js')}}"></script>
-  <script src="{{ asset('js/bootstrap.js') }}"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script> --}}
-  {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> --}}
- <!-- Fin de Scripts -->
+  <script src="{{ asset('js/app.js') }}"></script>
+  <!-- End Scripts -->
 </body>
 
 </html>
