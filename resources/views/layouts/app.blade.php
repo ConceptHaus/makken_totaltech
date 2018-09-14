6a077f9a41986fc67f6ade0c5b68dd5f02d1cfce
Lizbeth Luna <liz@concepthaus.mx>
2018-09-14 11:38:02 -0500

    Se agrego cambios home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,71 +2,74 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Total Tech</title>
-    <meta name="description" content="DESCRIPTION">
-    <!-- CSRF Token -->
-   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="UTF-8">
+  <title>Total Tech</title>
+  <meta name="description" content="DESCRIPTION">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Estilos -->
-   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <!-- Estilos -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
 
- <body>
+<body>
 
-<!-- Inicio de Menú -->
-<nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-<a class="navbar-brand" href="index.html"> <img class="logo-menu" src="{{ asset('img/logos/puedescontodo-logo.svg') }}"></a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-  <ul class="navbar-nav">
-    <li class="nav-item active">
-      <a class="nav-link" href="index.html">DINÁMICA</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#ganadores">GANADORES</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">PARTICIPA</a>
-    </li>
-  </ul>
-</div>
-</nav>
-<!-- Fin del Menú -->
+  <!-- Inicio de Menú -->
+  <nav class="navbar navbar-home navbar-expand-lg navbar-dark">
+    <a class="navbar-brand navbar-brand-home" href="{{ url('/') }}"> 
+      <img class="logo-menu" src="{{ asset('img/logos/puedescontodo-logo.svg') }}">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{ url('/')}}">DINÁMICA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/#ganadores')}}">GANADORES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/login')}}">PARTICIPA</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- Fin del Menú -->
 
-{{-- Content --}}
+  {{-- Content --}}
 
-@yield('content')
+  @yield('content')
 
-{{-- End Content --}}
+  {{-- End Content --}}
 
-<!-- Footer -->
-<footer>
-  <div class="container pt-4 pb-4">
-    <div class="row align-items-center">
-      <div class="col-lg-2">
-        SOPORTE
-55-555-5555
-info@correo.com
-      </div>
-      <div class="col-lg-2">
-        LEGALES
-Aviso de Privacidad
-Términos y Condiciones
-      </div>
-      <div class="col-lg-6 text-center">
-        WWW.MUNDOCEYS.COM
-      </div>
-      <div class="col-lg-2">
-        <img class="img-fluid"src="{{ asset('img/logos/ceys-logo.svg') }}" alt="Ceys">
+  <!-- Footer -->
+  <footer>
+    <div class="container pt-4 pb-4">
+      <div class="row align-items-center">
+        <div class="col-lg-2">
+          SOPORTE
+          55-555-5555
+          info@correo.com
+        </div>
+        <div class="col-lg-2">
+          LEGALES
+          Aviso de Privacidad
+          Términos y Condiciones
+        </div>
+        <div class="col-lg-6 text-center">
+          WWW.MUNDOCEYS.COM
+        </div>
+        <div class="col-lg-2">
+          <img class="img-fluid" src="{{ asset('img/logos/ceys-logo.svg') }}" alt="Ceys">
+        </div>
       </div>
     </div>
-  </div>
-</footer>
-<!-- Fin Footer -->
+  </footer>
+  <!-- Fin Footer -->
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}"></script>
