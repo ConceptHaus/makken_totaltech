411489a652175e366c83fac22583fbd2e86f7632
Sergio Ramos <sergirams@gmail.com>
2018-09-18 16:48:17 -0500

    cambios

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Establecimiento;
+
+class ApiController extends Controller
+{
+    public function getCP($cp){
+        $datosCP = CodigosPostales::where('CodigoPostal','=',$cp)->first();
+        
+        if(!$datosCP){
+            $datosCP['error'] = 'Este código no es válido o no existe';
+            return response($datosCP,400);
+        }
+        return response($datosCP,200);
+
+    }
+
+    public function getEstablecimientos(){
+        $establecimientos = Establecimiento::all();
+
+        return response($establecimientos,200);
+    }
+}
