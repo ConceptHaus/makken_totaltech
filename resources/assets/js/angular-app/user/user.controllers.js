38ca8690b6e31822e226d67aa88eb0d5d5ae5232
Sergio Ramos <sergirams@gmail.com>
2018-09-18 18:43:43 -0500

    cambios

--- resources/assets/js/angular-app/user/user.controllers.js
+++ resources/assets/js/angular-app/user/user.controllers.js
@@ -49,7 +49,8 @@ app.controller("userCtrl", function($scope, UserFactory, $http, $window, Upload,
             console.log(err);
         })
     }
-   $scope.addTicker = function(ticket){
+   $scope.addTicket = function(ticket){
+       console.log(ticket);
     swal({
         title:"Espera...",
         text:"Estamos enviando tu ticket.",
@@ -61,10 +62,10 @@ app.controller("userCtrl", function($scope, UserFactory, $http, $window, Upload,
     })
     .then(function(res){
         swal.close();
-        console.log(res);
+        console.log(res.data);
     },function(err){
         swal.close();
-        console.log(err);
+        console.log(err.data);
     })
 
    }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Auth
 Route::post('/login','Auth\LoginController@login');
 Route::post('/registro','Auth\RegisterController@registroUser');
 Route::get('/logout', 'Auth\LoginController@logout');
@@ -26,6 +27,9 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/ticket', 'HomeController@addNewTicket');
 
-//Funcionalidades
+//Catálogo
 Route::get('/api/v1/cp/{cp}','Apicontroller@getCP');
 Route::get('/api/v1/establecimientos','ApiController@getEstablecimientos');
+
+//Funcionalidades
+Route::post('/ticket','UserController@creaTicketUser');
