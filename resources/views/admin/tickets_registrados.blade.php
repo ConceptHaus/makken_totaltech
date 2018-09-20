72d1ce6f8f863ef5ef9666e1b818f9c1ef70e252
Lizbeth Luna <liz@concepthaus.mx>
2018-09-19 19:32:37 -0500

    Se agregaron ultimos cambios v2

--- resources/views/admin/tickets_registrados.blade.php
+++ resources/views/admin/tickets_registrados.blade.php
@@ -31,86 +31,87 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr ng-repeat="ganador in ganadores | filter:search">
-                                <td><% ganador.id_ganador %></td>
+                            <tr ng-repeat="ticket in tickets | filter:search">
+                                <td><% ticket.id_ticket %></td>
                                 <td>
-                                    <% ganador.user.nombre %>
+                                    <% ticket.user.nombre %> <% ticket.user.apellido %>
                                 </td>
-                                <td><% ganador.user.telefono %></td>
-                                <td><% ganador.ticket.monto %></td>
-                                <td><img class="mr-3" src="{{ asset('img/logos/chedraui.png') }}" alt="image" width="90" /></td>
-                                <td><% ganador.ticket.created_at %></td>
+                                <td><% ticket.no_ticket %></td>
+                                <td><% ticket.monto %></td>
                                 <td>
-                                    <span data-toggle="modal" data-target="#modalGanador<% ganador.id_ganador %>">
+                                    <% ticket.establecimiento.nombre %>
+                                    {{-- <img class="mr-3" src="{{ asset('img/logos/chedraui.png') }}" alt="image" width="90" /> --}}
+                                </td>
+                                <td><% ticket.created_at %></td>
+                                <td>
+                                    <span data-toggle="modal" data-target="#modalGanador<% ticket.id_ticket %>">
                                         <a class="text-light font-20 mr-2" data-container="body" data-toggle="popover" data-trigger="hover" data-placement="top" data-content="Elige como ganador a este usuario.">
                                             <i class="fa fa-star-o"></i>
                                         </a>
                                     </span>
                                     <!-- START MODAL GANADOR -->
-                                        <div class="modal fade" id="modalGanador" tabindex="-1" role="dialog" aria-labelledby="modalGanador" aria-hidden="true">
-                                            <div class="modal-dialog" role="document">
-                                                <form class="modal-content">
-                                                    <div class="modal-header p-4">
-                                                        <h5 class="modal-title">GANADOR</h5>
-                                                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                                                            <span aria-hidden="true">×</span>
-                                                        </button>
+                                    <div class="modal fade" id="modalGanador<% ticket.id_ticket %>" tabindex="-1" role="dialog" aria-labelledby="modalGanador" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <form class="modal-content">
+                                                <div class="modal-header p-4">
+                                                    <h5 class="modal-title">GANADOR</h5>
+                                                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">×</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body p-4">
+                                                    <div class="form-group mb-4">
+                                                        <p>Selecciona la semana correspondiente al ganador.</p>
+                                                        <select class="form-control" id="type-filter" title="Semanas ganadores" ng-model="semana" data-style="btn-solid">
+                                                            <option value="1">1 Semana</option>
+                                                            <option value="2">2 Semana</option>
+                                                            <option value="3">3 Semana</option>
+                                                            <option value="4">4 Semana</option>
+                                                        </select>
                                                     </div>
-                                                    <div class="modal-body p-4">
-                                                        <div class="form-group mb-4">
-                                                            <p>Selecciona la semana correspondiente al ganador.</p>
-                                                            <select class="selectpicker show-tick form-control" id="type-filter" title="Semanas ganadores" data-style="btn-solid">
-                                                                <option>1 Semana</option>
-                                                                <option>2 Semana</option>
-                                                                <option>3 Semana</option>
-                                                                <option>4 Semana</option>
-                                                                <option value="">Todas</option>
-                                                            </select>
-                                                        </div>
-                                                        <div class="form-group mb-4">
-                                                            <h5 class="text-center">No. Ticket</h5>
-                                                            <h4 class="text-center">545676 - 647423</h4>
-                                                        </div>
-                                                        <p>¿Está seguro que los datos son correctos y estas eligiendo este ticket como ganador?</p>
+                                                    <div class="form-group mb-4">
+                                                        <h5 class="text-center">No. Ticket</h5>
+                                                        <h4 class="text-center"><% ticket.no_ticket %></h4>
                                                     </div>
-                                                    <div class="modal-footer bg-primary-50">
-                                                        <button class="btn btn-primary btn-rounded mr-3" type="button">Acepto</button>
-                                                        <button class="btn btn-rounded mr-3" type="button" data-dismiss="modal" aria-label="Close">Cancelar</button>
-                                                    </div>
-                                                </form>
-                                            </div>
+                                                    <p>¿Está seguro que los datos son correctos y estas eligiendo este ticket como ganador?</p>
+                                                </div>
+                                                <div class="modal-footer bg-primary-50">
+                                                    <button class="btn btn-primary btn-rounded mr-3" type="button">Acepto</button>
+                                                    <button class="btn btn-rounded mr-3" type="button" data-dismiss="modal" aria-label="Close">Cancelar</button>
+                                                </div>
+                                            </form>
                                         </div>
-                                        <!-- END MODAL GANADOR -->
+                                    </div>
+                                    <!-- END MODAL GANADOR -->
                                     <span data-toggle="modal" data-target="#modalTicket">
                                         <a class="text-light font-20" data-container="body" data-toggle="popover" data-trigger="hover" data-placement="top" data-content="Ver imagen de ticket.">
                                             <img src="{{ asset('img/icons/camera-blue.svg') }}" width="22">
                                         </a>
                                     </span>
+                                    <!-- START MODAL TICKET -->
+                                    <div class="modal fade" id="modalTicket<% ticket.id_ganador %>" tabindex="-1" role="dialog" aria-labelledby="modalTicket" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <form class="modal-content">
+                                                <div class="modal-header p-4">
+                                                    <h5 class="modal-title">TICKET</h5>
+                                                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">×</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body p-4">
+                                                    <img class="mr-3" ng-src="<% ticket.url %>" alt="image"/>
+                                                    {{-- {{ asset('img/elements/ticket-default-2.jpg') }} --}}
+                                                </div>
+                                                <div class="modal-footer bg-primary-50">
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <!-- END MODAL TICKET -->
                                 </td>
                             </tr>
                         </tbody>
-                    </table>
-
-                    <!-- START MODAL TICKET -->
-                    <div class="modal fade" id="modalTicket" tabindex="-1" role="dialog" aria-labelledby="modalTicket" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <form class="modal-content">
-                                <div class="modal-header p-4">
-                                    <h5 class="modal-title">TICKET</h5>
-                                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">×</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body p-4">
-                                    <img class="mr-3" src="{{ asset('img/elements/ticket-default-2.jpg') }}" alt="image"/>
-                                </div>
-                                <div class="modal-footer bg-primary-50">
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- END MODAL TICKET -->
-                    
+                    </table>                    
                 </div>
             </div>
         </div>
