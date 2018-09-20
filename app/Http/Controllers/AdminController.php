b197722cdc1cea4190b1ccfc10ea8179c48df2fa
Sergio Ramos <sergirams@gmail.com>
2018-09-20 15:30:16 -0500

    cambios

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -133,7 +133,7 @@ class AdminController extends Controller {
         $ganador->id_semana = intval($request->id_semana);
         $ganador->id_premio = 1;
         $ticket = Ticket::where('id_ticket',$ganador->id_ticket)->first();
-        $ganador->save();
+        
             $json['success'] = 'success_ganador';
             $json['ticket'] = $ticket->no_ticket;
             return response($json, 200);
