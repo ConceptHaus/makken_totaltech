84203231b055193ce88bc5786de7c2d912158ec3
fcastaneda9725 <f.castanedatrujillo@gmail.com>
2018-10-05 08:42:54 -0500

    Dashboard terminado

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,10 +4,12 @@
     <!-- START PAGE CONTENT-->
     <div class="page-content fade-in-up">
         <div class="row">
+          {{-- Reporte Semanal --}}
           <div class="col-xl-6">
             <h1>Reporte Semanal</h1>
             <p>30 de julio al 5 de agosto</p>
           </div>
+          {{-- Fin Reporte Semanal --}}
           <div class="col-xl-4">
               <div class="ibox">
                   <div class="ibox-body">
@@ -44,9 +46,8 @@
                         <div class="flexbox mb-3">
                             <span class="flexbox">
                                 <span class="btn-icon-only btn-circle bg-success-50 text-success mr-3"><i class="ti-briefcase"></i></span>
-                                <span>BUSINESS PLAN</span>
                             </span>
-                            <span class="h3 mb-0 text-success font-strong ml-2">+890</span>
+                            <span class="h3 mb-0 text-success font-strong ml-2">46</span>
                         </div>
                     </div>
                 </div>
@@ -54,37 +55,18 @@
             {{-- Fin Panel 1 --}}
             {{-- Panel 2 --}}
               <div class="col-xl-4">
-                  <div class="ibox ibox-fullheight" style="min-height:480px;">
+                  <div class="ibox">
                       <div class="ibox-head">
-                          <div class="ibox-title">SUBSCRIPTIONS</div>
-                          <div class="ibox-tools">
-                              <a class="font-18" href="javascript:;"><i class="ti-control-record"></i></a>
-                          </div>
+                          <div class="ibox-title">Top Tiendas</div>
                       </div>
                       <div class="ibox-body">
                           <div class="flexbox mb-3">
-                              <span class="flexbox">
-                                  <span class="btn-icon-only btn-circle bg-primary-50 text-primary mr-3"><i class="ti-cup"></i></span>
-                                  <span>BASIC PLAN</span>
-                              </span>
-                              <span class="h3 mb-0 text-primary font-strong ml-2">+1250</span>
-                          </div>
-                          <div class="flexbox mb-3">
-                              <span class="flexbox">
-                                  <span class="btn-icon-only btn-circle bg-success-50 text-success mr-3"><i class="ti-briefcase"></i></span>
-                                  <span>BUSINESS PLAN</span>
-                              </span>
-                              <span class="h3 mb-0 text-success font-strong ml-2">+890</span>
-                          </div>
-                          <div class="flexbox mb-3">
-                              <span class="flexbox">
-                                  <span class="btn-icon-only btn-circle bg-pink-50 text-pink mr-3"><i class="ti-gift"></i></span>
-                                  <span>EXPANDED PLAN</span>
-                              </span>
-                              <span class="h3 mb-0 text-pink font-strong ml-2">+720</span>
-                          </div>
-                          <div class="abs-bottom">
-                              <canvas id="chart_1" style="height:220px;"></canvas>
+                              <ol>
+                                <li>Walmart</li>
+                                <li>Comercial Mexicana</li>
+                                <li>HEB</li>
+                                <li>HOWES</li>
+                              </ol>
                           </div>
                       </div>
                   </div>
@@ -92,42 +74,31 @@
               {{-- Fin Panel 2 --}}
               {{-- Panel 3 --}}
                 <div class="col-xl-4">
-                    <div class="ibox ibox-fullheight" style="min-height:480px;">
+                    <div class="ibox">
                         <div class="ibox-head">
-                            <div class="ibox-title">SUBSCRIPTIONS</div>
-                            <div class="ibox-tools">
-                                <a class="font-18" href="javascript:;"><i class="ti-control-record"></i></a>
-                            </div>
+                            <div class="ibox-title">Ticket de compra con mayor monto</div>
                         </div>
                         <div class="ibox-body">
                             <div class="flexbox mb-3">
-                                <span class="flexbox">
+                                {{-- <span class="flexbox"> --}}
                                     <span class="btn-icon-only btn-circle bg-primary-50 text-primary mr-3"><i class="ti-cup"></i></span>
-                                    <span>BASIC PLAN</span>
-                                </span>
-                                <span class="h3 mb-0 text-primary font-strong ml-2">+1250</span>
+                                    <span>Soriana Izatapalapa SUC. 262</span>
+                                {{-- </span> --}}
+
                             </div>
-                            <div class="flexbox mb-3">
-                                <span class="flexbox">
-                                    <span class="btn-icon-only btn-circle bg-success-50 text-success mr-3"><i class="ti-briefcase"></i></span>
-                                    <span>BUSINESS PLAN</span>
-                                </span>
-                                <span class="h3 mb-0 text-success font-strong ml-2">+890</span>
-                            </div>
-                            <div class="flexbox mb-3">
-                                <span class="flexbox">
-                                    <span class="btn-icon-only btn-circle bg-pink-50 text-pink mr-3"><i class="ti-gift"></i></span>
-                                    <span>EXPANDED PLAN</span>
-                                </span>
-                                <span class="h3 mb-0 text-pink font-strong ml-2">+720</span>
-                            </div>
-                            <div class="abs-bottom">
-                                <canvas id="chart_1" style="height:220px;"></canvas>
+                            <div class="flexbox mb-3" style="justify-content: center !important;">
+                              <span class="h3 mb-0 text-primary font-strong ml-2">$1250</span>
                             </div>
                         </div>
                     </div>
                 </div>
                 {{-- Fin Panel 3 --}}
+                  {{-- Reporte Semanal --}}
+                  <div class="col-xl-8 coments">
+                    <h1>Comentarios</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  </div>
+                  {{-- Fin Reporte Semanal --}}
           </div>
         </div>
     <!-- END PAGE CONTENT-->
