1eebc063a1633b4843242f531324db0ef5ddb621
moscarinrin <moscarin94@gmail.com>
2019-10-09 16:49:15 -0500

    Responsive

--- public/js/forgotPassword.controllers.js
+++ public/js/forgotPassword.controllers.js
@@ -36,32 +36,12 @@
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
 /******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
 /******/ 	};
 /******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
@@ -77,8 +57,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
-/******/
+/******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 5);
@@ -87,10 +66,6 @@
 /******/ ({
 
 /***/ "./resources/assets/js/angular-app/forgotPassword/forgotPassword.controllers.js":
-/*!**************************************************************************************!*\
-  !*** ./resources/assets/js/angular-app/forgotPassword/forgotPassword.controllers.js ***!
-  \**************************************************************************************/
-/*! no static exports found */
 /***/ (function(module, exports) {
 
 /**
@@ -99,86 +74,83 @@
  * Description
  */
 var app = angular.module('forgotPasswordController', ['angularApp']);
-app.controller("ForgotPasswordCtrl", function ($scope, ForgotFactory, $http, $window, Upload, CSRF_TOKEN) {
-  $scope.forgot2 = function (correo) {
-    //console.log(user);
-    swal({
-      title: 'Espera...',
-      text: 'Estamos verificando tus datos.',
-      imageUrl: '../img/icons/Spinner-1s-200px.gif',
-      showConfirmButton: false
-    });
-    ForgotFactory.forgot(correo).then(function (res) {
-      swal({
-        title: "¡Todo bien!",
-        text: "Revisa tu correo electrónico",
-        confirmButtonText: 'Iniciar Sesión'
-      }).then(function (result) {
-        if (result.value) {
-          $window.location.href = "/login";
-        }
-      });
-    }, function (err) {
-      console.log(err);
-      swal({
-        title: "¡Ups algo salio mal!",
-        text: err.data.fail.correo,
-        confirmButtonText: 'Regresar',
-        showCancelButton: true,
-        cancelButtonText: 'Intentar nuevamente'
-      }).then(function (result) {
-        if (result.value) {
-          $window.location.href = "/";
-        }
-      });
-    });
-  };
 
-  $scope.reset = function (resetP) {
-    console.log(resetP);
-    swal({
-      title: 'Espera...',
-      text: 'Estamos verificando tus datos.',
-      imageUrl: '../img/icons/Spinner-1s-200px.gif',
-      showConfirmButton: false
-    });
-    ForgotFactory.reset2(resetP).then(function (res) {
-      swal({
-        title: "¡Todo bien!",
-        text: "Tu contraseña ha sido restablecida",
-        confirmButtonText: 'Ir a Mi Cuenta'
-      }).then(function (result) {
-        if (result.value) {
-          $window.location.href = "/home";
-        }
-      });
-    }, function (err) {
-      console.log(err);
-      swal({
-        title: "¡Ups algo salio mal!",
-        text: "Intenta nuevamente",
-        confirmButtonText: 'Regresar',
-        showCancelButton: true,
-        cancelButtonText: 'Intentar nuevamente'
-      }).then(function (result) {
-        if (result.value) {
-          $window.location.href = "/";
-        }
-      });
-    });
-  };
+app.controller("ForgotPasswordCtrl", function ($scope, ForgotFactory, $http, $window, Upload, CSRF_TOKEN) {
+    $scope.forgot2 = function (correo) {
+        //console.log(user);
+        swal({
+            title: 'Espera...',
+            text: 'Estamos verificando tus datos.',
+            imageUrl: '../img/icons/Spinner-1s-200px.gif',
+            showConfirmButton: false
+        });
+        ForgotFactory.forgot(correo).then(function (res) {
+            swal({
+                title: "¡Todo bien!",
+                text: "Revisa tu correo electrónico",
+                confirmButtonText: 'Iniciar Sesión'
+            }).then(function (result) {
+                if (result.value) {
+                    $window.location.href = "/login";
+                }
+            });
+        }, function (err) {
+            console.log(err);
+            swal({
+                title: "¡Ups algo salio mal!",
+                text: err.data.fail.correo,
+                confirmButtonText: 'Regresar',
+                showCancelButton: true,
+                cancelButtonText: 'Intentar nuevamente'
+            }).then(function (result) {
+                if (result.value) {
+                    $window.location.href = "/";
+                }
+            });
+        });
+    };
+
+    $scope.reset = function (resetP) {
+        console.log(resetP);
+        swal({
+            title: 'Espera...',
+            text: 'Estamos verificando tus datos.',
+            imageUrl: '../img/icons/Spinner-1s-200px.gif',
+            showConfirmButton: false
+        });
+        ForgotFactory.reset2(resetP).then(function (res) {
+            swal({
+                title: "¡Todo bien!",
+                text: "Tu contraseña ha sido restablecida",
+                confirmButtonText: 'Ir a Mi Cuenta'
+            }).then(function (result) {
+                if (result.value) {
+                    $window.location.href = "/home";
+                }
+            });
+        }, function (err) {
+            console.log(err);
+            swal({
+                title: "¡Ups algo salio mal!",
+                text: "Intenta nuevamente",
+                confirmButtonText: 'Regresar',
+                showCancelButton: true,
+                cancelButtonText: 'Intentar nuevamente'
+            }).then(function (result) {
+                if (result.value) {
+                    $window.location.href = "/";
+                }
+            });
+        });
+    };
 });
 
 /***/ }),
 
 /***/ 5:
-/*!********************************************************************************************!*\
-  !*** multi ./resources/assets/js/angular-app/forgotPassword/forgotPassword.controllers.js ***!
-  \********************************************************************************************/
-/*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/eiichimatsuo/Desktop/ConceptHaus/makken_totaltech/resources/assets/js/angular-app/forgotPassword/forgotPassword.controllers.js */"./resources/assets/js/angular-app/forgotPassword/forgotPassword.controllers.js");
+module.exports = __webpack_require__("./resources/assets/js/angular-app/forgotPassword/forgotPassword.controllers.js");
 
 
 /***/ })
