d0f59e4e08aadacd522aad949f97d6ab585133b5
Sergio Ramos <sergirams@gmail.com>
2020-03-12 13:20:38 -0600

    cambios makken marzo

--- resources/views/components/dinamica.blade.php
+++ resources/views/components/dinamica.blade.php
@@ -19,7 +19,8 @@
       <img src="{{asset('img/icons/paso_2.svg')}}" class="dinamica__pasos-number" alt="Paso 2">
       <img src="{{ asset('img/icons/paso2.svg') }}" class="dinamica__pasos-img" alt="Paso 2">
       <div class="text-center">
-        <h5 class="dinamica__h5 mt-3">REGISTRA EL MAYOR NÚMERO DE TICKETS</h5>
+        <h5 class="dinamica__h5 mt-3">REGISTRA TODOS TUS TICKETS DE COMPRA</h5>
+        <h6 class="dinamica__h6">(con monto mínimo de $100)</h6>
       </div>
     </div>
     <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 my-4 wow zoomIn" data-wow-offset="135"
@@ -32,9 +33,11 @@
     </div>
   </div>
   <div class="row text-center justify-content-center">
-    <div class="col-lg-8 col-md-12 mt-5 mb-5">
+    <div class="col-lg-8 col-md-12 mt-5 mb-5 mx-2">
       <h6 class="dinamica__h6">MIENTRAS MÁS COMPRAS, MÁS OPORTUNIDADES TIENES DE GANAR.</h6>
+      <h6 class="dinamica__h6">EL PARTICIPANTE QUE REGISTRE EL MAYOR MONTO ACUMULADO GANARÁ UN SCOOTER.</h6>
       <h4 class="dinamica__h4">¡RECUERDA QUE TODAS LAS COMPRAS DE LA SEMANA PARTICIPAN!</h4>
+      <h6 class="dinamica__h6">*Límitado a un scooter por participante</h6>
       <h6 class="dinamica__h6">Vigencia del 13 de marzo al 8 de mayo 2020</h6>
     </div>
   </div>
