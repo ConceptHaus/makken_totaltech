6d08a2ca755e146126b30d951a33f1c7748fe55a
Sergio Ramos <sergirams@gmail.com>
2020-03-12 23:19:24 -0600

    smooth scroll

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -160,7 +160,7 @@
 
     <!-- Scripts de Javi -->
     <script src="{{ asset('js/wow.min.js') }}"></script>
-    <script src="{{ asset('js/smooth-scroll.js') }}"></script>
+    {{-- <script src="{{ asset('js/smooth-scroll.js') }}"></script> --}}
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
       integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
