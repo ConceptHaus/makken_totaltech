9f1c750e27642bf9e082c8c9c9777df8c5f06af4
Sergio Ramos <sergirams@gmail.com>
2020-03-20 15:48:05 -0600

    links

--- resources/views/components/participantes.blade.php
+++ resources/views/components/participantes.blade.php
@@ -4,36 +4,43 @@
       <h2 class="participantes__h2 text-center">ESTABLECIMIENTOS PARTICIPANTES</h2>
       <div class="row">
         <div class="col-6">
-          <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
-             src="{{ asset('img/logos/walmart.png') }}" alt="Walmart">
+          <a href=" https://super.walmart.com.mx/productos?Ntt=ceys" target="_blank"><img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
+            src="{{ asset('img/logos/walmart.png') }}" alt="Walmart"></a>
         </div>
         <div class="col-6">
           <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
              src="{{ asset('img/logos/soriana.png') }}" alt="Soriana">
         </div>
         <div class="col-6">
-          <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
-             src="{{ asset('img/logos/lacomer.png') }}" alt="La comer">
+          <a href="https://www.lacomer.com.mx/lacomer/goBusqueda.action?succId=14&ver=mislistas&succFmt=100&criterio=ceys#/ceys" target="_blank"><img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
+          src="{{ asset('img/logos/lacomer.png') }}" alt="La Comer"></a>
         </div>
         <div class="col-6">
-          <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
-             src="{{ asset('img/logos/casacravioto.png') }}" alt="Casa Cravioto">
+          <a href="https://casacraviotoeshop.com/catalogsearch/result/?q=ceys" target="_blank"><img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
+            src="{{ asset('img/logos/casacravioto.png') }}" alt="Casa Cravioto"></a>
+          
         </div>
         <div class="col-6">
           <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
              src="{{ asset('img/logos/heb.png') }}" alt="HEB">
         </div>
         <div class="col-6">
-          <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
-             src="{{ asset('img/logos/sodimac.png') }}" alt="Lowe's">
+          <a href="https://www.sodimac.com.mx/sodimac-mx/search/?Ntt=ceys" target="_blank">
+            <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
+            src="{{ asset('img/logos/sodimac.png') }}" alt="Sodimac">
+          </a>
         </div>
         <div class="col-6">
-          <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
+          <a href="https://www.homedepot.com.mx/SearchDisplay?categoryId=&storeId=10351&catalogId=10101&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=20&searchTerm=ceys#facet:&productBeginIndex:0&facetLimit:&orderBy:&pageView:grid&minPrice:&maxPrice:&pageSize:&" target="_blank">
+            <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
              src="{{ asset('img/logos/homedepot.png') }}" alt="Home Depot">
+          </a>
         </div>
         <div class="col-6">
-          <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
+          <a href=" https://www.chedraui.com.mx/search?text=ceys" target="_blank">
+            <img class="participantes__tiendas-img wow fadeInDown my-3" data-wow-duration="1s"
              src="{{ asset('img/logos/chedraui.png') }}" alt="Chedraui">
+          </a>
         </div>
       </div>
     </div>
