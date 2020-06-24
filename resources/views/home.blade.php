b10b65d2f8e561e186f2a52429f99cdea73f657a
Sergio Ramos <sergirams@gmail.com>
2020-06-24 18:37:11 -0500

    Modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -92,28 +92,5 @@ $now = new DateTime();
   </div>
 </div>
 <!-- Fin Tickets -->
- <!-- Modal -->
- <div class="modal fade" id="covidModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-  <div class="modal-content">
-      <div class="modal-header">
-          <h5 class="modal-title font-italic text-center" id="exampleModalLabel">¡AVISO IMPORTANTE!</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-          </button>
-      </div>
-      <div class="modal-body">
-          <p class="m-4">Debido a la contingencia vivida por el brote COVID-19 considerada causa de fuerza mayor,
-              a partir del corte del 3 de abril de 2020 (semana 3 de participación), se suspende temporalmente la promoción
-          </p>
-          <img src="{{asset('img/images/montate.png')}}" class="img-fluid" alt="Ceys Logo">
-          <p class="m-4">¡Visita el sitio con frecuencia para seguir participando una vez que se normalicen las activiades!</p>
-          <p class="aviso m-4">*Los ganadores de las semanas 1,2 y 3 están confirmados y serán contactados para la entrega de sus premios</p>
-      </div>
-      <div class="modal-footer">
-      <button type="button" class="btn btn-primary btn-block" data-dismiss="modal">Aceptar</button>
-      </div>
-  </div>
-  </div>
-</div> 
+ 
 @endsection
