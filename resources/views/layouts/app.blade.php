c436d52316f8c6774418f1f3e973b5343f8975c2
Sergio Ramos <sergirams@gmail.com>
2020-06-24 18:41:02 -0500

    Modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -170,12 +170,12 @@
           </button>
       </div>
       <div class="modal-body">
-          <p class="m-4">Debido a la contingencia vivida por el brote COVID-19 considerada causa de fuerza mayor,
+          <p class="m-4 text-light">Debido a la contingencia vivida por el brote COVID-19 considerada causa de fuerza mayor,
               a partir del corte del 3 de abril de 2020 (semana 3 de participación), se suspende temporalmente la promoción
           </p>
           <img src="{{asset('img/images/montate.png')}}" class="img-fluid" alt="Ceys Logo">
-          <p class="m-4">¡Visita el sitio con frecuencia para seguir participando una vez que se normalicen las activiades!</p>
-          <p class="aviso m-4">*Los ganadores de las semanas 1,2 y 3 están confirmados y serán contactados para la entrega de sus premios</p>
+          <p class="m-4 text-light">¡Visita el sitio con frecuencia para seguir participando una vez que se normalicen las activiades!</p>
+          <p class="aviso m-4 text-light">*Los ganadores de las semanas 1,2 y 3 están confirmados y serán contactados para la entrega de sus premios</p>
       </div>
       <div class="modal-footer">
       <button type="button" class="btn btn-primary btn-block" data-dismiss="modal">Aceptar</button>
