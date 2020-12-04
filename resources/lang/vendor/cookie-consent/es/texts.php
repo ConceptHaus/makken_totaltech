caa7c652cb456f268bf99fa1d66d2397a0dc0bea
Sergio Ramos <sergio.ramos@circusmarketing.com>
2020-12-03 23:25:36 -0600

    cookies 3

---
+++ resources/lang/vendor/cookie-consent/es/texts.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'alert_accept' => 'ACEPTO',
+    'alert_settings' => 'Configurar',
+    'alert_text' => 'En ganaconceys.com.mx utilizamos cookies. Al navegar en este sitio, acepta nuestra política de cookies y aviso de privacidad.',
+    'alert_title' => 'Esta página web usa cookies',
+    'setting_analytics' => 'Analytical cookies',
+    'setting_analytics_text' => 'nos permite analizar el uso del sitio web y mejorar la experiencia del visitante.',
+    'setting_essential' => 'Functional cookies',
+    'setting_essential_text' => 'son cookies esenciales que garantizan que el sitio web funcione correctamente y que se guarden sus preferencias (por ejemplo, idioma, región).',
+    'setting_marketing' => 'Marketing cookies',
+    'setting_marketing_text' => 'Permítanos personalizar su experiencia y enviarle contenido y ofertas relevantes, en este sitio web y otros sitios web.',
+    'settings_accept_all' => 'Aceptar todas las cookies',
+    'settings_cancel' => 'Cancelar',
+    'settings_close' => 'Cerrar',
+    'settings_save' => 'Guardar',
+    'settings_title' => 'Configuración de cookies',
+    'settings_text' => 'Nuestro sitio web mantiene tres niveles de cookies. Puede ajustar sus preferencias en cualquier momento. Si desea obtener más información sobre qué son las cookies y qué cookies recopilamos, lea nuestro',
+];
