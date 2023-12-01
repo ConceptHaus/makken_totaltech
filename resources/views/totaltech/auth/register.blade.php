9cfefd61e4316bb6e5a41e9caba72256c27243cc
leslye@concepthaus.mx <leslye@concepthaus.mx>
2023-12-01 15:42:54 -0600

    se inhabilita la promoción

--- resources/views/totaltech/auth/register.blade.php
+++ resources/views/totaltech/auth/register.blade.php
@@ -24,76 +24,8 @@
             <div class="panel panel-register">
                 <div class="panel-body text-center">
                     <h3>REGÍSTRATE</h3>
-                    <form class="form-horizontal form-ajust">
-                        {{ csrf_field() }}
-                        <input ng-model="user.registro_admin" name="registro_admin" ng-init="user.registro_admin = 0"
-                            type="text" hidden>
-                        <input ng-model="user.id_promo" name="id_promo" ng-init="user.id_promo = 2"
-                            type="text" hidden>    
-                        <input ng-model="user.admin" name="registro_admin" ng-init="user.admin = 0" type="text" hidden>
-                        <div class="form-row">
-                            <div class="col form-group">
-                                <input ng-class="{'invalido': errors.nombre }" ng-model="user.nombre" id="name"
-                                    type="text" class="form-control" name="name" placeholder="Nombre" required
-                                    autofocus >
-                                <p ng-cloak class="error-form" ng-if="errors.nombre"><% errors.nombre[0] %></p>
-                            </div>
-                            <div class="col form-group">
-                                <input ng-class="{'invalido': errors.apellido }" ng-model="user.apellido" id="lastname"
-                                    type="text" class="form-control" name="lastname" placeholder="Apellido" required
-                                    autofocus>
-                                <p ng-cloak class="error-form" ng-if="errors.apellido"><% errors.apellido[0] %></p>
-                            </div>
-                        </div>
-
-                        <div class="form-row">
-                            <div class="col form-group">
-                                <input ng-class="{'invalido': errors.telefono }" ng-model="user.telefono" id="phone"
-                                    type="text" class="form-control" name="phone" mask="(99) 9999-9999" 
-                                    placeholder="Teléfono" required autofocus>
-                                <p ng-cloak class="error-form" ng-if="errors.telefono"><% errors.telefono[0] %></p>
-                            </div>
-                            <div class="col form-group">
-                                <input ng-class="{'invalido': errorCP }" ng-model="user.cp" id="cp" type="text"
-                                    class="form-control" name="cp" ng-blur="getCP(user.cp)" placeholder="C.P."
-                                    mask="999999" required autofocus>
-                                <p ng-cloak class="error-form" ng-if="errorCP"><% errorCP %></p>
-                            </div>
-                        </div>
-                        
-
-                        <div class="form-group">
-                            <input ng-class="{'invalido': errors.correo }" ng-model="user.correo" id="email"
-                                type="email" class="form-control" name="email" placeholder="Correo" required>
-                            <p ng-cloak class="error-form" ng-if="errors.correo"><% errors.correo[0] %></p>
-                        </div>
-
-                        <div class="form-group">
-                            <input ng-class="{'invalido': errors.password }" ng-model="user.password" id="password"
-                                type="password" class="form-control" name="password" placeholder="Contraseña" required>
-                            <p ng-cloak class="error-form" ng-if="errors.password"><% errors.password[0] %></p>
-                        </div>
-
-                        <div class="form-group">
-                            <input ng-class="{'invalido': errors.password_confirmation }"
-                                ng-model="user.password_confirmation" id="password-confirm" type="password"
-                                class="form-control" name="password_confirmation" placeholder="Confirmar contraseña"
-                                required>
-                            <p ng-cloak class="error-form" ng-if="errors.password_confirmation">
-                                <% errors.password_confirmation[0] %></p>
-                        </div>
-                        
-                        <div class="form-group">
-                            <button type="submit" ng-click="register(user,confirm)" 
-                                class="btn btn-submit">ENTRAR</button>
-
-                        </div>
-                        <!-- Campo oculto -->
-                        
-                        <div class="mb-2">
-                            <a class="a-login mb-2" href="{{ route('totaltech.login') }}">Ya tengo una cuenta.</a>
-                        </div>
-                    </form>
+                    <p clas="texxt-center">¡La promoción llego a su fin!</p>
+               
                 </div>
             </div>
 
