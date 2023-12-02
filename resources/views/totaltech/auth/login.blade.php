24835a8b7c5c76d87c6267c2420469124b0d3aca
leslye@concepthaus.mx <leslye@concepthaus.mx>
2023-12-02 17:13:18 -0600

    se vuelve a activar prmocion totaltech

--- resources/views/totaltech/auth/login.blade.php
+++ resources/views/totaltech/auth/login.blade.php
@@ -33,8 +33,47 @@ $now = new DateTime();
                     <form class="form-horizontal">
                         {{ csrf_field() }}
                         <h3>INICIAR SESIÓN</h3>
-                        <p class="text-center">¡La promoción ya termino!</p>
-                        
+                        <div class="form-group">
+                            <input ng-model="user.id_promo" name="id_promo" ng-init="user.id_promo = 2"
+                            type="text" hidden>    
+                            <input ng-class="{'invalido': errors['error'] || errors['email'] }" ng-model="user.email"
+                                id="email" type="email" class="form-control" name="email" placeholder="Usuario" required
+                                autofocus>
+                            <div ng-cloak ng-if="errors['email']" class="alert alert-warning" role="alert">
+                                <% errors['email'][0] %> 
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <input ng-class="{'invalido': errors['error'] || errors['password'] }"
+                                ng-model="user.password" id="password" type="password" class="form-control"
+                                name="password" placeholder="Contraseña" required>
+                        </div>
+                        <div ng-cloak ng-if="errors['error']" class="alert alert-warning" role="alert">
+                            <% errors['error'] %>
+                        </div>
+
+                        <div ng-cloak ng-if="errors['password']" class="alert alert-warning" role="alert">
+                            <% errors['password'][0] %>
+                        </div>
+                        <div class="form-group content-actions text-center no-margin align-items-start pt-0">
+
+                           <a class="a-forget-password" href="{{ route('totaltech.password.request') }}">
+                                ¿Olvidaste tu contraseña?
+                            </a>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <button ng-click="login(user)" class="btn btn-submit">ENTRAR</button>
+                            </div>
+                            <p class="my-3 text-center">¿No tienes cuenta?
+                                @if($date <= $now)
+
+                                <a class="registrate" href="{{ route('totaltech.register') }}">Regístrate.</a>
+                                @else
+                                <a class="disabled registrate">Regístrate.</a>
+                                @endif
+                            </p>
                         </div>
                     </form>
                 </div>
